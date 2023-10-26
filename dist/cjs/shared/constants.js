e7ba037569c70603931827daf85655947be5b2fa
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-10-26 19:14:15 +0330

    refactor: change structure

--- dist/cjs/shared/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AUDIO_CHANNEL_COUNT = exports.MAX_FRAME_RATE = exports.AUDIO_SAMPLE_SIZE = exports.MAX_VIDEO_LENGTH = void 0;
-exports.MAX_VIDEO_LENGTH = 1;
-exports.AUDIO_SAMPLE_SIZE = 100;
-exports.MAX_FRAME_RATE = 30;
-exports.AUDIO_CHANNEL_COUNT = 2;
