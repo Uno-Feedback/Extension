dac72b2b585d135a87b3ad91bbfd9585cde5dc9d
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-10-28 16:25:31 +0330

    docs: add new initializer example

--- js/lib/uno-js/initialerExample.js
+++ js/lib/uno-js/initialerExample.js
@@ -10,38 +10,49 @@
 // /* End: Add for Initialize */
 /**
  * Bottom of file */
-
 // /* Start: Add for Initialize */
-// const options = {
-//   user: {
-//     fullName: "John Doe",
-//     email: "j.doe@example.com",
-//     avatar: null,
-//   },
-//   autoSecretKey: "secret",
-//   callbacks: {
-//     onOpenWidget: () => console.log("Widget opened"),
-//     onCloseWidget: () => console.log("Widget closed"),
-//     onStartMask: () => console.log("Started mask"),
-//     onStopMask: () => console.log("Stopped mask"),
-//     onStartTimer: ({ second, minute, hours }) =>
-//       console.log(
-//         "Started recording timer: " + second + ":" + minute + ":" + hours
-//       ),
-//     onStopTimer: () => console.log("Stopped recording timer"),
-//     onStartRecording: () => console.log("Started recording!"),
-//     onStopRecording: () => console.log("Stopped recording!"),
-//     onSubmit: () => console.log("Submitted!"),
-//     onError: () => console.log("Error!"),
-//   },
-//   subscriptionData: {
-//     apiKey: "#API_KEY#",
-//     requestUrl: "#REQUEST_URL#",
-//   },
-//   startButtonId: "start-btn",
-// };
-// unoJS.initialize(options);
-// setTimeout(() => {
-//   startButton.click();
-// }, 100);
+// chrome.storage.sync.get(null, function (items) {
+//   const requiredData = {
+//     fullName: null,
+//     autoSecretKey: null,
+//     requestUrl: null,
+//     apiKey: null,
+//   };
+//   Object.keys(items).forEach((item) => {
+//     requiredData[item] = items[item];
+//   });
+//   const options = {
+//     user: {
+//       fullName: requiredData.fullName,
+//       email: "j.doe@example.com",
+//       avatar: null,
+//     },
+//     autoSecretKey: requiredData.autoSecretKey,
+//     callbacks: {
+//       onOpenWidget: () => console.log("Widget opened"),
+//       onCloseWidget: () => console.log("Widget closed"),
+//       onStartMask: () => console.log("Started mask"),
+//       onStopMask: () => console.log("Stopped mask"),
+//       onStartTimer: ({ second, minute, hours }) =>
+//         console.log(
+//           "Started recording timer: " + second + ":" + minute + ":" + hours
+//         ),
+//       onStopTimer: () => console.log("Stopped recording timer"),
+//       onStartRecording: () => console.log("Started recording!"),
+//       onStopRecording: () => console.log("Stopped recording!"),
+//       onSubmit: () => console.log("Submitted!"),
+//       onError: () => console.log("Error!"),
+//     },
+//     subscriptionData: {
+//       apiKey: requiredData.apiKey,
+//       requestUrl: requiredData.requestUrl,
+//     },
+//     startButtonId: "start-btn",
+//   };
+//   console.log({ options });
+//   unoJS.initialize(options);
+//   setTimeout(() => {
+//     startButton.click();
+//   }, 100);
+// });
 // /* End: Add for Initialize */
