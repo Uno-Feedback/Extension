741ca1e34601252eb747bc69332436f44109111d
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-10-28 16:24:01 +0330

    fix: sync problem of get storage

--- js/lib/uno-js/bundle.js
+++ js/lib/uno-js/bundle.js
@@ -1827,49 +1827,49 @@
   };
   var unoJS = new UnoJSBuilder();
   /* Start: Add for Initialize */
-  // Get data from storage
-  const requiredData = {
-    fullName: null,
-    autoSecretKey: null,
-    requestUrl: null,
-    apiKey: null,
-  };
   chrome.storage.sync.get(null, function (items) {
+    const requiredData = {
+      fullName: null,
+      autoSecretKey: null,
+      requestUrl: null,
+      apiKey: null,
+    };
     Object.keys(items).forEach((item) => {
       requiredData[item] = items[item];
     });
+    const options = {
+      user: {
+        fullName: requiredData.fullName,
+        email: "j.doe@example.com",
+        avatar: null,
+      },
+      autoSecretKey: requiredData.autoSecretKey,
+      callbacks: {
+        onOpenWidget: () => console.log("Widget opened"),
+        onCloseWidget: () => console.log("Widget closed"),
+        onStartMask: () => console.log("Started mask"),
+        onStopMask: () => console.log("Stopped mask"),
+        onStartTimer: ({ second, minute, hours }) =>
+          console.log(
+            "Started recording timer: " + second + ":" + minute + ":" + hours
+          ),
+        onStopTimer: () => console.log("Stopped recording timer"),
+        onStartRecording: () => console.log("Started recording!"),
+        onStopRecording: () => console.log("Stopped recording!"),
+        onSubmit: () => console.log("Submitted!"),
+        onError: () => console.log("Error!"),
+      },
+      subscriptionData: {
+        apiKey: requiredData.apiKey,
+        requestUrl: requiredData.requestUrl,
+      },
+      startButtonId: "start-btn",
+    };
+    console.log({ options });
+    unoJS.initialize(options);
+    setTimeout(() => {
+      startButton.click();
+    }, 100);
   });
-  const options = {
-    user: {
-      fullName: requiredData.fullName,
-      email: "j.doe@example.com",
-      avatar: null,
-    },
-    autoSecretKey: requiredData.autoSecretKey,
-    callbacks: {
-      onOpenWidget: () => console.log("Widget opened"),
-      onCloseWidget: () => console.log("Widget closed"),
-      onStartMask: () => console.log("Started mask"),
-      onStopMask: () => console.log("Stopped mask"),
-      onStartTimer: ({ second, minute, hours }) =>
-        console.log(
-          "Started recording timer: " + second + ":" + minute + ":" + hours
-        ),
-      onStopTimer: () => console.log("Stopped recording timer"),
-      onStartRecording: () => console.log("Started recording!"),
-      onStopRecording: () => console.log("Stopped recording!"),
-      onSubmit: () => console.log("Submitted!"),
-      onError: () => console.log("Error!"),
-    },
-    subscriptionData: {
-      apiKey: requiredData.apiKey,
-      requestUrl: requiredData.requestUrl,
-    },
-    startButtonId: "start-btn",
-  };
-  unoJS.initialize(options);
-  setTimeout(() => {
-    startButton.click();
-  }, 100);
   /* End: Add for Initialize */
 })();
